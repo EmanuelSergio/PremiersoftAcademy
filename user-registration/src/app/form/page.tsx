06814fdcceb65d866d010c762abd0307be908dd0
Emanuel Sergio Girardi <emanuelsergiogirardi@gmail.com>
2024-12-04 14:44:34 -0300

    form com hook OK

---
+++ user-registration/src/app/form/page.tsx
@@ -0,0 +1,9 @@
+import Form from "@/components/Form";
+
+export default function FormPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-100">
+      <Form />
+    </main>
+  );
+}
