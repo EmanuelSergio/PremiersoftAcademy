7beb431b029ef68678951ddc64b54aca573e4bb8
Emanuel Sergio Girardi <emanuelsergiogirardi@gmail.com>
2024-12-11 14:11:32 -0300

    api rest com typescript

--- BACKEND/Express/meu-projeto-express/index.js
+++ BACKEND/Express/meu-projeto-express/index.js
@@ -1,8 +1,8 @@
-const express = require("express");
 const app = express();
 const userRoutes = require("./routes/users");
 app.use("/users", userRoutes);
-const PORT = 3000;
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
 
 //rota principal
 
